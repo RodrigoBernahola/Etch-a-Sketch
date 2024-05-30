06aa041bfbfd720ae52e904cc9509e93ac167606
rodrigol <rodrigobernahola28@gmail.com>
2024-05-30 16:33:54 -0300

    Se agregaran cambios en detalles de las funciones

--- logic.js
+++ logic.js
@@ -3,16 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Función para crear la cuadrícula
     function createGrid(size) {
+        opacidad += 0.1;
+
         for (let i = 0; i < size * size; i++) {
+            let randomRed = Math.floor(Math.random() * 256).toString(16).padStart(2,'0');
+            let randomGreen = Math.floor(Math.random() * 256).toString(16).padStart(2,'0');
+            let randomBlue = Math.floor(Math.random() * 256).toString(16).padStart(2,'0');
             let divItem = document.createElement('div');
-            let gridWidth = 960 / size;
+            const gridWidth = 960 / size;
             divItem.style.cssText = `
                 width: ${gridWidth}px;
                 height: ${gridWidth}px;
-                background-color: #c6c;
+                background-color: #${randomRed}${randomGreen}${randomBlue};
                 flex: 0 0 ${gridWidth}px;
+                opacity: ${opacidad};
             `;
             divItem.className = "Paint";
+            console.table(divItem.style)
             container.appendChild(divItem);
         }
     }
@@ -31,6 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Inicializar la cuadrícula por defecto
+    let opacidad = 0;
     createGrid(16);
 
     let resetButton = document.querySelector('#btn');
@@ -40,7 +48,6 @@ document.addEventListener('DOMContentLoaded', () => {
             res = parseInt(prompt('Ingrese la cantidad de filas y columnas a dibujar (maximo 100):'));
         } while (0 > res || res > 100);
 
-        console.log(res);
         resetPaintedButtons(res);
     });
 
